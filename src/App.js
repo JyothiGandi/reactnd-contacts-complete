fd6ace52534c8399b4cc8b8689aba99a5311033b
JyothiGandi <jyothi.gandi5494@gmail.com>
2018-11-30 08:52:10 +0530

    Feat: list, search and delete contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
+  state = {
+    contacts : []
+  };
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then(contacts => {
+        this.setState({contacts});
+      })
+  }
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter(c => c.id !== contact.id)
+    }));
+    ContactsAPI.remove(contact);
+  };
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact}/>
       </div>
     )
   }
